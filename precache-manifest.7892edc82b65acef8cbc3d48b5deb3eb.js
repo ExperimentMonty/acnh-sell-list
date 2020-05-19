a7141b95b49b18352afa569d335674a408bf51e9
John Montgomery <johnbmontgomery@gmail.com>
2020-05-19 12:34:56 -0400

    Updates

---
+++ precache-manifest.7892edc82b65acef8cbc3d48b5deb3eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3f85be72a1a4191a97d6e65729a7cb",
+    "url": "/acnh-sell-list/index.html"
+  },
+  {
+    "revision": "df003c0d71089cfaef24",
+    "url": "/acnh-sell-list/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "446a40c7165638be3aa5",
+    "url": "/acnh-sell-list/static/js/2.44dd3638.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/acnh-sell-list/static/js/2.44dd3638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df003c0d71089cfaef24",
+    "url": "/acnh-sell-list/static/js/main.cb20abad.chunk.js"
+  },
+  {
+    "revision": "a58543d9e30614cb284b",
+    "url": "/acnh-sell-list/static/js/runtime-main.0813ea44.js"
+  }
+]);
